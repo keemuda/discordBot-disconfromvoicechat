f7d5aaecc397a7f6c244432794b7bf30defcc72f
keemuda <75478830+keemuda@users.noreply.github.com>
2024-09-14 00:00:32 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(TOKEN);
