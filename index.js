fe18c874be51e511077d12676208ed136ca088d0
keemuda <kee.pan@hotmail.com>
2024-09-16 02:40:24 +0700

    Create 'kickme'command to immediately disconnect a user from voice chat

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { Client, GatewayIntentBits } from 'discord.js';
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -10,6 +10,16 @@ client.on('interactionCreate', async interaction => {
 
   if (interaction.commandName === 'ping') {
     await interaction.reply('Pong!');
+  }
+  if(interaction.commandName === 'kickme'){
+    const member = interaction.member;
+
+    if(member.voice.channel){
+      await member.voice.disconnect();
+      await interaction.reply(`remove ${member.displayName} from ${member.voice.channel.name}`)
+    }else{
+      await interaction.reply("You not in voice channel!");
+    }
   }
 });
 
