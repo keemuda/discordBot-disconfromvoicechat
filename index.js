90ffaef907f6583a556573a86a6b83f8519eb6fe
keemuda <75478830+keemuda@users.noreply.github.com>
2024-09-23 21:53:15 +0700

    add dockerfile,dockerignore clear unused command

--- index.js
+++ index.js
@@ -19,17 +19,7 @@ client.on('interactionCreate', async interaction => {
   if (interaction.commandName === 'ping') {
     await interaction.reply('Pong!');
   }
-  if (interaction.commandName === 'kickme') {
-    const member = interaction.member;
-
-    if (member.voice.channel) {
-      await member.voice.disconnect();
-      await interaction.reply(`remove ${member.displayName} from ${member.voice.channel.name}`)
-    } else {
-      await interaction.reply("You not in voice channel!");
-    }
-  }
-
+  
   if (interaction.commandName === 'voicekick') {
    await voicekickCommand.execute(interaction,scheduledDisconnects);
   }
